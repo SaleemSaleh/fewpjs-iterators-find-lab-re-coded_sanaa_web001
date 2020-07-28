a8f1fe2eb2364ed785a870640da9b6063747b5e9
Saleem Saleh <salem1234saleem@gmail.com>
2020-07-28 18:32:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function testFunc() {
 
 function superbowlWin(sa) {
   let win = sa.find( s => s.result === "W" )
-  if (win === -1 ) {
+  if (win == -1 ) {
     return undefined ;
   }
   return win.year ;
